314a4abcaa21aff8704399aedc3065a49d70dc07
Kartik Luke Singh <kartikdgr8@gmail.com>
2013-07-05 06:40:12 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youdo::Application.config.secret_token = 'ac333ccb9219838234bdae3204b1428ca7c774acc473221d35b8505229975b54c0c18fcf4f16d10231b5f4392b210692d3813ed30c995f504c0a06fe078fe057'
